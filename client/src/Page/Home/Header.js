a4f32801cdb1205c892d3af75d7a4730bfc8371b
unknown <datp1907@gmail.com>
2023-09-17 16:41:47 +0700

    up

--- client/src/Page/Home/Header.js
+++ client/src/Page/Home/Header.js
@@ -5,14 +5,14 @@ import { LogoTiktok, logo, logoFb, logoTiktokMobile } from '../../assets'
 const Header = () => {
   return (
     <div className='flex shadow-md h-[70px] w-full shrink-0 items-center bg-gradient-to-r from-teal-200 to-pink-200 bg-center bg-no-repeat bg-cover bg-scroll overflow-hidden max-h-full px-6'>
-      <a className="flex gap-2 w-4/12 items-center " href='https://www.facebook.com/profile.php?id=100012882123870' >
-        <span className='text-base mobile:hidden font-semibold text-pink-600' >Liên hệ</span>
+      <a className="flex gap-2 w-4/12 items-center " target="_blank" href='https://www.facebook.com/profile.php?id=100012882123870' >
+        <span className='text-base mobile:hi font-semibold text-pink-600' >Liên hệ</span>
         <img className='h-8 rounded-sm' src={logoFb} />
       </a>
       <div className="flex justify-center  w-4/12">
         <img src={logo} className='w-40 rounded-full' />
       </div>
-      <a className="flex justify-end w-4/12" href='https://www.tiktok.com/@_hoamuoigio_' >
+      <a className="flex justify-end w-4/12" target="_blank"  href='https://www.tiktok.com/@tamconcophat' >
         <span className='mobile:hidden'> {LogoTiktok} </span>
         <img className='mobile:block w-10 h-10 rounded-lg hidden' src={logoTiktokMobile} />
       </a>
